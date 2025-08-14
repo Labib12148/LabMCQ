fcc2bd7edd0503ef65eae66d5d680179d8837463
Labib12148 <labib12148@gmail.com>
2025-08-14 20:22:25 +0600

    fixed option issues

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,9 @@ export default defineConfig({
   },
   server: {
     host: true,
-    allowedHosts: [
-      'localhost',
-      '127.0.0.1',
-    ]
+    port: 5173,
+    strictPort: true,
+    hmr: { clientPort: 443 }
   },
   build: {
     rollupOptions: {
