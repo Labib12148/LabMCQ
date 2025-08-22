0e651bf519108399c361f4758541bba26963d98f
Sadman Labib <153620824+Labib12148@users.noreply.github.com>
2025-08-23 03:52:22 +0600

    load global styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import App from '@/App.jsx';
 import '@/index.css';
+import '@/styles/styles.css';
 
 const rootElement = document.getElementById('root');
 
